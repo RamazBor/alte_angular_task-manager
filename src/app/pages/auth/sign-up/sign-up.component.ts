dda79b517ea0fd35b7849b7a1ca6d11c156b4034
Ramaz Borchashvili <ramazborchashvili00@gmail.com>
2024-06-13 20:55:35 +0400

    add sign up page design and validate form

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header/header.component";
+import { HeaderComponent } from '../../../components/header/header.component';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOption, MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
-    selector: 'alte-sign-up',
-    standalone: true,
-    templateUrl: './sign-up.component.html',
-    styleUrl: './sign-up.component.scss',
-    imports: [HeaderComponent]
+  selector: 'alte-sign-up',
+  standalone: true,
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss',
+  imports: [
+    HeaderComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatLabel,
+    MatOption,
+    MatButtonModule
+  ],
 })
 export class SignUpComponent {
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  signUp() {
+    this.form.markAllAsTouched();
+    }
 }
