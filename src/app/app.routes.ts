c1cdfaaeb71d38fe08905543c3a125d64cfb58c1
Ramazi <100078934+RamazBor@users.noreply.github.com>
2024-06-16 21:03:29 +0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignInComponent } from './pages/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   {
@@ -18,6 +19,8 @@ export const routes: Routes = [
     component: SignUpComponent
   },
   {
+    path: 'home-page',
+    component: HomePageComponent
   },
   {
     path: 'tasks',
@@ -28,3 +31,4 @@ export const routes: Routes = [
     component: ProjectsComponent
   }
 ];
+
